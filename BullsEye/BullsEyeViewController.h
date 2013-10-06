5a2e966bbcb2585924aeb08d4af7d0f61b80f5c4
Paris Kapsouros <paris.kapsouros@gmail.com>
2013-10-07 02:45:42 +0300

    before qbout

--- BullsEye/BullsEyeViewController.h
+++ BullsEye/BullsEyeViewController.h
@@ -8,10 +8,16 @@
 
 #import <UIKit/UIKit.h>
 
-@interface BullsEyeViewController : UIViewController
+@interface BullsEyeViewController : UIViewController <UIAlertViewDelegate>
+
+@property (nonatomic, weak) IBOutlet UISlider *slider;
+@property (nonatomic, weak) IBOutlet UILabel *targetLabel;
+@property (nonatomic, weak) IBOutlet UILabel *scoreLabel;
+@property (nonatomic, weak) IBOutlet UILabel *roundLabel;
 
 - (IBAction)showAlert;
-
+- (IBAction)startOver;
 - (IBAction)sliderMoved: (UISlider *) slider;
+
 
 @end
--- BullsEye/BullsEyeViewController.m
+++ BullsEye/BullsEyeViewController.m
@@ -14,12 +14,16 @@
 
 @implementation BullsEyeViewController {
     int _currentValue;
+    int _targetValue;
+    int _score;
+    int _round; //default 0
 }
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    _currentValue = 50;
+    [self startNewGame];
+    [self updateLabels];
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -29,19 +33,63 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (IBAction)showAlert {
 
-    NSString *message = [ NSString stringWithFormat:@"The current value of the slider is %d", _currentValue];
+- (void)startNewGame {
+    _score = 0;
+    _round = 0;
+    [self startNewRound];
+}
+
+- (void)startNewRound {
+    _targetValue = 1 + arc4random_uniform(100);
+    _currentValue = 50;
+    _round += 1;
+    self.slider.value = _currentValue;
+}
+
+- (IBAction)startOver {
+    [self startNewGame];
+    [self updateLabels];
+}
+
+- (void)updateLabels {
+    self.targetLabel.text = [NSString stringWithFormat:@"%d",_targetValue];
+    self.scoreLabel.text = [NSString stringWithFormat:@"%d",_score];
+    self.roundLabel.text = [NSString stringWithFormat:@"%d", _round];
+}
+
+- (IBAction)showAlert {
+    
+    int difference = abs(_currentValue - _targetValue);
+    
+    int points = 100 - difference;
+    
+    NSString *message = [NSString stringWithFormat:@"You scored %d points", points];
+    NSString *title;
+    
+    if (difference == 0) {
+        title = @"Perfect";
+        points += 100;
+    } else if (difference <5) {
+        if (difference == 1) {
+            points += 50;
+        }
+        title = @"You almost had it there!";
+    } else if (difference <10) {
+        title = @"Pretty good!";
+    } else {
+        title = @"Not even close...";
+    }
+    
+    _score += points;
     
     UIAlertView *alertView = [[UIAlertView alloc]
-                              initWithTitle:@"Hello world"
+                              initWithTitle:title
                               message:message
-                              delegate:nil
+                              delegate:self
                               cancelButtonTitle:@"OK"
-                              otherButtonTitles:nil,
-                              nil
-                            ];
-    
+                              otherButtonTitles:nil
+                              ];
     [alertView show];
 }
 
@@ -49,5 +97,10 @@
     _currentValue = lroundf(slider.value);
 }
 
+
+- (void)alertView:(UIAlertView *)alertView didDismissWithButtonIndex:(NSInteger)buttonIndex {
+    [self startNewRound];
+    [self updateLabels];
+}
 
 @end
